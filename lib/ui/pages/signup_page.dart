77b898cc8f0cbfbfd285cac818e5bedcf8a4fe2f
PlattenEcho <akunsekolahdika@gmail.com>
2023-08-12 18:00:03 +0700

    update bagian dika

--- lib/ui/pages/signup_page.dart
+++ lib/ui/pages/signup_page.dart
@@ -24,99 +24,102 @@ class _SignupPage extends State<SignupPage> {
       ),
     );
 
+    dynamic parentWidth = MediaQuery.of(context).size.width;
     return Scaffold(
         backgroundColor: kWhiteColor,
-        body: Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage(
-                      'assets/bg_login.png',
-                    ),
-                    fit: BoxFit.cover)),
-            child: Flex(
-                direction: Axis.vertical,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Expanded(flex: 1, child: SizedBox()),
-
-                  //Logo
-                  const Expanded(
-                      flex: 0,
-                      child: Image(
-                        image: AssetImage('assets/logo_text_small.png'),
-                      )),
-
-                  //TextField Email
-                  const Expanded(
-                    flex: 0,
-                    child: NewForm(
-                        nama: "Email",
-                        hintText: "Masukkan Email",
-                        obscureText: false,
-                        topPadding: 10,
-                        bottomPadding: 5,
-                        horizontalPadding: 25),
-                  ),
-
-                  //TextField Password
-                  const Expanded(
-                    flex: 0,
-                    child: NewForm(
-                        nama: "Password",
-                        hintText: "Masukkan Password",
-                        obscureText: true,
-                        topPadding: 5,
-                        bottomPadding: 10,
-                        horizontalPadding: 25),
-                  ),
-
-                  //TextField Konfirmasi Password
-                  const Expanded(
-                    flex: 0,
-                    child: NewForm(
-                        nama: "Konfirmasi Password",
-                        hintText: "Masukkan konfirmasi password",
-                        obscureText: true,
-                        topPadding: 5,
-                        bottomPadding: 10,
-                        horizontalPadding: 25),
-                  ),
-
-                  Expanded(
+        body: SingleChildScrollView(
+          child: Container(
+              width: parentWidth,
+              height: parentWidth,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(
+                        'assets/bg_login.png',
+                      ),
+                      fit: BoxFit.cover)),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Expanded(child: SizedBox()),
+        
+                    //Logo
+                    const Expanded(
+                        flex: 1,
+                        child: Image(
+                          image: AssetImage('assets/logo_text_small.png'),
+                        )),
+        
+                    //TextField Email
+                    const Expanded(
                       flex: 1,
-                      child: Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 30),
-                        padding: const EdgeInsets.only(top: 20),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Button(
-                                text: "Daftar",
-                                textColor: kWhiteColor,
-                                startColor: kPrimaryColor,
-                                endColor: kPrimary2Color,
-                                onPressed: () {
-                                  Navigator.pushNamed(context, '/counseling');
+                      child: NewForm(
+                          nama: "Email",
+                          hintText: "Masukkan Email",
+                          obscureText: false,
+                          topPadding: 10,
+                          bottomPadding: 5,
+                          horizontalPadding: 25),
+                    ),
+        
+                    //TextField Password
+                    const Expanded(
+                      flex: 1,
+                      child: NewForm(
+                          nama: "Password",
+                          hintText: "Masukkan Password",
+                          obscureText: true,
+                          topPadding: 5,
+                          bottomPadding: 10,
+                          horizontalPadding: 25),
+                    ),
+        
+                    //TextField Konfirmasi Password
+                    const Expanded(
+                      flex: 0,
+                      child: NewForm(
+                          nama: "Konfirmasi Password",
+                          hintText: "Masukkan konfirmasi password",
+                          obscureText: true,
+                          topPadding: 5,
+                          bottomPadding: 10,
+                          horizontalPadding: 25),
+                    ),
+        
+                    Expanded(
+                        flex: 1,
+                        child: Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 30),
+                          padding: const EdgeInsets.only(top: 20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Button(
+                                  text: "Daftar",
+                                  textColor: kWhiteColor,
+                                  startColor: kPrimaryColor,
+                                  endColor: kPrimary2Color,
+                                  onPressed: () {
+                                    Navigator.pushNamed(context, '/counseling');
+                                  })
+                            ],
+                          ),
+                        )),
+        
+                    RichText(
+                        text: TextSpan(
+                            style: blackTextStyle.copyWith(color: Colors.black),
+                            children: <TextSpan>[
+                          const TextSpan(text: "Sudah punya akun? "),
+                          TextSpan(
+                              text: "Masuk di sini",
+                              style: blackTextStyle.copyWith(color: kPrimary2Color),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  Navigator.pushReplacementNamed(context, '/login-page');
                                 })
-                          ],
-                        ),
-                      )),
-
-                  RichText(
-                      text: TextSpan(
-                          style: blackTextStyle.copyWith(color: Colors.black),
-                          children: <TextSpan>[
-                        const TextSpan(text: "Sudah punya akun? "),
-                        TextSpan(
-                            text: "Masuk di sini",
-                            style: blackTextStyle.copyWith(color: kPrimary2Color),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () {
-                                Navigator.pushReplacementNamed(context, '/login-page');
-                              })
-                      ])),
-                ])));
+                        ])),
+                  ])),
+        ));
   }
 }
