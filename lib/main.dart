0061193bedcd71013496c91456d38354482c5036
Resma Adi Nugroho <resmaadi@students.undip.ac.id>
2023-09-10 18:56:49 +0700

    feat: countdown on chat

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mentalove_app/ui/pages/chat_page.dart';
-import 'package:mentalove_app/ui/pages/chat_psikolog_page.dart';
 import 'package:mentalove_app/ui/pages/counseling.dart';
 import 'package:mentalove_app/ui/pages/daily_content.dart';
 import 'package:mentalove_app/ui/pages/detail.dart';
@@ -9,15 +8,15 @@ import 'package:mentalove_app/ui/pages/history.dart';
 import 'package:mentalove_app/ui/pages/landing_page.dart';
 import 'package:mentalove_app/ui/pages/login_page.dart';
 import 'package:mentalove_app/ui/pages/main_page.dart';
+import 'package:mentalove_app/ui/pages/pembayaran.dart';
 import 'package:mentalove_app/ui/pages/psikiater_page.dart';
 import 'package:mentalove_app/ui/pages/psikolog_page.dart';
 import 'package:mentalove_app/ui/pages/signup_page.dart';
 import 'package:mentalove_app/ui/pages/tell_us.dart';
-import 'package:mentalove_app/ui/pages/pembayaran.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'cubit/page_cubit.dart';
 import 'ui/pages/splash_screen.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -63,7 +62,6 @@ class MainApp extends StatelessWidget {
             const Pembayaran(terapisData: {}, mode: true),
         '/psikiater-page': (context) => const PsikiaterPage(),
         '/chat-page': (context) => const ChatPage(),
-        '/chat-psikologi': (context) => const ChatPsikologPage(),
       }),
     );
   }
